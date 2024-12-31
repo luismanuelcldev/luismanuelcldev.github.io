0eec258a309212f785ae1d600b90fe0f828f2ae4
Luis Manuel De La Cruz Ledesma <luismanueldelacruz0000@gmail.com>
2024-12-31 18:52:55 -0400

    Implementation of the Email Resend service

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -1,25 +1,26 @@
 // filepath: /C:/Users/LUIS MANUEL/Documents/Development/portfolio/pages/api/contact.ts
 import type { NextApiRequest, NextApiResponse } from 'next';
-import SparkPost from 'sparkpost';
+import { Resend } from 'resend';
+import dotenv from 'dotenv';
 
-const client = new SparkPost(process.env.SPARKPOST_API_KEY);
+dotenv.config();
+
+const resend = new Resend(process.env.API_KEY_EMAIL);
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'POST') {
         const { name, surname, email, phone, message } = req.body;
 
         const emailData = {
-            content: {
-                from: 'ledesmadelacruzluismanuel@gmail.com', // Correo verificado en SparkPost
-                subject: `Nuevo mensaje de ${name} ${surname}`,
-                html: `<h1>Nuevo mensaje de ${name} ${surname}</h1><p><strong>Email:</strong> ${email}</p><p><strong>Teléfono:</strong> ${phone}</p><p><strong>Mensaje:</strong> ${message}</p>`,
-            },
-            recipients: [{ address: 'ledesmadelacruzluismanuel@gmail.com' }],
+            from: 'ledesmadelacruzluismanuel@gmail.com',
+            to: 'ledesmadelacruzluismanuel@gmail.com',
+            subject: `Nuevo mensaje de ${name} ${surname}`,
+            html: `<h1>Nuevo mensaje de ${name} ${surname}</h1><p><strong>Email:</strong> ${email}</p><p><strong>Teléfono:</strong> ${phone}</p><p><strong>Mensaje:</strong> ${message}</p>`,
         };
 
         try {
-            await client.transmissions.send(emailData);
-            res.status(200).json({ message: 'Correo enviado con éxito' });
+            await resend.emails.send(emailData);
+            res.status(200).json({ message: 'Correo enviado exitosamente' });
         } catch (error) {
             res.status(500).json({ message: 'Error al enviar el correo', error });
         }
